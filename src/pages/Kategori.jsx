051e21aef99502557e5cc1a6f9543102d75caa0f
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-09-02 09:03:24 +0700

    upload file

---
+++ src/pages/Kategori.jsx
@@ -0,0 +1,3 @@
+export default function Kaegori() {
+    return <h2>Halaman Kategori</h2>;
+}
---
+++ src/pages/Tentang.jsx
@@ -0,0 +1,27 @@
+export default function Tentang() {
+    return (
+        <div className="container py-5 px-5">
+            <div className="row text-start">
+                <div className="col-10">
+                    <h2 className="fw-bold mb-3">Tentang</h2>
+                    <h4>🌷 Hai, Saya Andina Amanti Armitha Putri</h4>
+
+                <p>
+                        Siswi XII RPL 1, Saya memiliki ketertarikan di bidang teknologi dan pemrograman.
+                        Selain belajar, saya juga suka berwisata dan menikmati keindahan alam.
+                        Salah satu tempat yang ingin saya kunjungi adalah WISATA TELOMOYO,
+                        karena saya ingin melihat pemandangan alam yang indah dan menikmati suasana yang sejuk.
+
+                </p>
+
+                <p>
+                    Bagi saya, Telomoyo bukan hanya tentang sebuah tempat wisata,
+                    tetapi juga tentang menikmati keindahan alam, udara yang sejuk,
+                    dan menciptakan momen yang tak terlupakan. 🏔️☁️🌿✨
+
+                </p>
+                </div>
+            </div>
+        </div>
+    );
+}
