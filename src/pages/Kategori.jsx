fe7299fb476e8b40a2c4e135707bff0933a6ab0c
putriarmitha77-cmyk <putriarmitha77@gmail.com>
2026-09-23 13:28:01 +0700

    upload file

--- src/pages/Kategori.jsx
+++ src/pages/Kategori.jsx
@@ -1,3 +1,58 @@
-export default function Kaegori() {
-    return <h2>Halaman Kategori</h2>;
+import { useEffect, useState } from "react";
+
+export default function Kategori() {
+    const [kategori, setKategori] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getKategori = async () => {
+        try {
+            const res = await fetch("http://localhost:3001/kategori");
+            const data = await res.json();
+            setKategori(data);
+        } catch (err) {
+            console.error("Gagal fetch data:", err);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getKategori();
+    }, []);
+
+    if (loading) {
+        return <div className="container mt-4">Sedang memuat data...</div>
+    }
+
+    return (
+        <div className="container mt-4">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+                <h2>Daftar Kategori</h2>
+            </div>
+            <table className="table table-bordered table-striped">
+                <thead className="table-primary">
+                    <tr>
+                        <th>ID Kategori</th>
+                        <th>Nama Kategori</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {kategori.length > 0 ? (
+                        kategori.map((item) => (
+                            <tr key={item.id_kategori}>
+                                <td>{item.id_kategori}</td>
+                                <td>{item.kategori}</td>
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="2" className="text-center">
+                                Belum ada Kategori
+                        </td>
+                    </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    );
 }
